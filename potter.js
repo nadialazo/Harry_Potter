be3373802076f3dae58f3e69d8e9b7741393ff78
Nadia <nadialazo118@gmail.com>
2019-05-03 22:29:52 -0400

    version01

---
+++ potter.js
@@ -0,0 +1,27 @@
+function flexIt(){
+        let container = document.querySelector('.needFlex');
+        container.classList.toggle('flexIt');
+      
+}
+
+
+function emphasize(house) {
+    house = document.getElementsByClassName('');
+    for (i = 0; i < house.lenght; i++);
+    if (house[i].innerHTML === "gryffindor") {
+        house.classList.add('emphasize');
+    } 
+}
+    
+    
+    
+   /* if (house[i].innerHTML === "hufflepuff") {
+        house.classList.add('emphasize');
+    } if (house[i].innerHTML === "hufflepuff") {
+        house.classList.add('emphasize');
+    } if (house[i].innerHTML === "slytherin") {
+        house.classList.add('emphasize');
+    }
+
+}*/
+
